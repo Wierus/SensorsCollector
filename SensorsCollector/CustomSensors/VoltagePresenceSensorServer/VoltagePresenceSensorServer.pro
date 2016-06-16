eab1cd014c715693ccb4a70322428f6ac8373f36
Wierus <wierus2007@mail.ru>
2016-06-16 21:06:32 +0300

    VoltagePresenceSensorServer: добавлены скрипты для сборки deb-пакета

--- SensorsCollector/CustomSensors/VoltagePresenceSensorServer/VoltagePresenceSensorServer.pro
+++ SensorsCollector/CustomSensors/VoltagePresenceSensorServer/VoltagePresenceSensorServer.pro
@@ -8,12 +8,17 @@ CONFIG -= app_bundle
 
 TEMPLATE = app
 
+HEADERS += \
+    VoltagePresenceSensorServer.h
+
 SOURCES += \
     main.cpp \
     VoltagePresenceSensorServer.cpp
 
-HEADERS += \
-    VoltagePresenceSensorServer.h
+DISTFILES += \
+    package-create.sh \
+    package.control \
+    package.init
 
 INCLUDEPATH += \
     ../GenericSensorServer
@@ -26,3 +31,6 @@ LIBS += \
 
 target.path = /home/pi/SensorsCollector
 INSTALLS += target
+
+QMAKE_POST_LINK += cd $$_PRO_FILE_PWD_;
+QMAKE_POST_LINK += $$_PRO_FILE_PWD_/package-create.sh;
---
+++ SensorsCollector/CustomSensors/VoltagePresenceSensorServer/package-create.sh
@@ -0,0 +1,78 @@
+#########################
+# Переменные для сборки #
+#########################
+
+# Имя службы
+DAEMON_NAME="voltage-presence-sensor-server"
+# Версия службы
+DAEMON_VERSION="1.0.0"
+# Имя пакета (включая версию)
+PACKAGE_NAME=$DAEMON_NAME"_"$DAEMON_VERSION
+# Имя проекта в Qt
+QT_PROJECT_NAME="VoltagePresenceSensorServer"
+# Конфигурация проекта в Qt (Release/Debug)
+QT_PROJECT_CONFIGURATION="Release"
+# Директория файлов пакета для сборки (будет пересоздана)
+PACKAGE_TARGET_DIR=$QT_PROJECT_NAME-$QT_PROJECT_CONFIGURATION/$PACKAGE_NAME
+
+###################################
+# Инициализация пакета для сборки #
+###################################
+
+# Удаление предыдущих файлов
+rm --force $QT_PROJECT_NAME-$QT_PROJECT_CONFIGURATION/$PACKAGE_NAME*.deb
+rm --force $QT_PROJECT_NAME-$QT_PROJECT_CONFIGURATION/$PACKAGE_NAME*.tar.gz
+rm --force $QT_PROJECT_NAME-$QT_PROJECT_CONFIGURATION/$PACKAGE_NAME*.tar.xz
+
+# Удаление предыдущей директории с файлами пакета
+rm --recursive --force $PACKAGE_TARGET_DIR
+
+# Создание директории для файлов пакета
+mkdir --parents --verbose $PACKAGE_TARGET_DIR
+
+# Переход в директорию пакета
+cd $PACKAGE_TARGET_DIR
+
+# Подготовка файлов пакета
+dh_make --createorig --single --packagename $PACKAGE_NAME --yes
+
+# Возврат в исходную директорию
+cd ../..
+
+#########################################
+# Копирование файлов в пакет для сборки #
+#########################################
+
+# Копирование файлов в директорию "debian"
+cp package.control  $PACKAGE_TARGET_DIR/debian/control
+cp package.init     $PACKAGE_TARGET_DIR/debian/$DAEMON_NAME.init
+
+# Создание в пакете директорий
+mkdir --parents --verbose $PACKAGE_TARGET_DIR/debian/$DAEMON_NAME/usr/local/bin
+
+# Копирование файлов в пакет (файлы должны существовать)
+cp $QT_PROJECT_NAME-$QT_PROJECT_CONFIGURATION/$QT_PROJECT_NAME $PACKAGE_TARGET_DIR/debian/$DAEMON_NAME/usr/local/bin/$DAEMON_NAME
+
+# Установка флага "+x" для запускаемых файлов
+chmod +x $PACKAGE_TARGET_DIR/debian/$DAEMON_NAME/usr/local/bin/$DAEMON_NAME
+
+###########################
+# Сборка пакета из файлов #
+###########################
+
+# Переход в директорию пакета
+cd $PACKAGE_TARGET_DIR
+
+# Выполнение команд сборки пакета
+fakeroot dh_testroot
+fakeroot dh_fixperms
+fakeroot dh_makeshlibs
+fakeroot dh_shlibdeps --dpkg-shlibdeps-params=--ignore-missing-info -l/mnt/rasp-pi-rootfs/lib/arm-linux-gnueabihf:/mnt/rasp-pi-rootfs/usr/lib/arm-linux-gnueabihf:/mnt/rasp-pi-rootfs/usr/local/qt5pi/lib
+fakeroot dh_installinit
+fakeroot dh_installdeb
+fakeroot dh_gencontrol
+fakeroot dh_md5sums
+fakeroot dh_builddeb
+
+# Возврат в исходную директорию
+cd ../..
